919cbedf23254326e13234d108dc453e3f54a037
Adam Engebretson <adam.engebretson@instructure.com>
2021-04-03 14:55:30 -0500

    Initial Commit

---
+++ main/game.go
@@ -0,0 +1,30 @@
+components {
+  id: "playerFactory"
+  component: "/player/factory.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "game"
+  component: "/main/game.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
